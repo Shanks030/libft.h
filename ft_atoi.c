180946388bfe5a58503455b73176ab05c038c11e
Shanks030 <amineqaos1@gmail.com>
2023-10-08 13:28:13 +0100

    libft.h

---
+++ ft_atoi.c
@@ -0,0 +1,30 @@
+int ft_atoi(char *str)
+{
+	int i;
+	int result;
+	int sign;
+
+	i = 0;
+	result = 0;
+	sign = 1;
+	while (str[i] <= 32)
+	{
+		i++;
+	}
+	if (str[i] == '-')
+	{
+		sign *= -1;
+		i++;
+	}
+	if (str[i] == '+')
+	{
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result *= 10;
+		result += str[i] - '0';
+		i++;
+	}
+	return (result * sign);
+}
